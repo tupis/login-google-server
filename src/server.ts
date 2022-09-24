b7d84eed084df68af1267ec7a32ea069f0fc0e1e
Tupis <joaoh.tupinamba@gmail.com>
2022-09-24 15:58:11 -0300

    feat: connection to database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import * as express from "express";
 import router from "./routes/routes";
+import connection from "./config/config";
+import { json, urlencoded } from "body-parser";
+
+connection
+  .sync()
+  .then(() => console.log("Connected with sucess to database"))
+  .catch(() => console.log("Connection failed to database"));
 
 const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
 app.use(router);
 
 app.listen(3000, () => console.log("Server Online!"));
