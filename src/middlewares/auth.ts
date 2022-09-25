1e7095856d0ed16e7906cf46bf64b24751e51c90
Tupis <joaoh.tupinamba@gmail.com>
2022-09-25 19:01:49 -0300

    feat: create authenticator token

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,21 @@
+// import { NextFunction, Request, Response } from "express";
+// import { RequestHandler } from "express";
+import { verify } from "jsonwebtoken";
+import authConfig from "../config/jwt";
+
+const AuthToken = (req: any, res: any, next: any): void | object => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ Error: "Sem token!" });
+  }
+  verify(token, authConfig.secret, (error: any, decoded: any) => {
+    if (error) {
+      return res.status(402).json({ Error: "Token Inválido" });
+    } else {
+      req.user = decoded;
+      next();
+    }
+  }) as any;
+};
+
+export default AuthToken;
