880eda1a31b85e0fac7299a81f8fb9025d0262c5
Tupis <joaoh.tupinamba@gmail.com>
2022-09-27 18:41:38 -0300

    feat: create  routes login/register

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { login } from "../controller/users_google";
+import { loginGoogle } from "../controller/users_google";
+import { login, register } from "../controller/users";
+
+// MiddleWares
+import isRegisteredUser from "../middlewares/isRegisteredUser";
 // import AuthToken from "../middlewares/auth";
 
 const router = Router();
 
-router.post("/login/google", login);
+router.post("/login/google", loginGoogle);
+router.post("/register", isRegisteredUser, register);
+router.post("/login", login);
 
 export default router;
