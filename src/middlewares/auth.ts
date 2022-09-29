7bdb9ecd667a1a2768ae43979e105adfbb02aa1c
Tupis <joaoh.tupinamba@gmail.com>
2022-09-29 01:09:20 -0300

    fix: remove import dont used

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,3 @@
-// import { NextFunction, Request, Response } from "express";
-// import { RequestHandler } from "express";
 import { verify } from "jsonwebtoken";
 import authConfig from "../config/jwt";
 
