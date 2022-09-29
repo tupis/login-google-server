1469fd5c85bad531bf7725135915e50d6fd85773
Tupis <joaoh.tupinamba@gmail.com>
2022-09-29 10:02:43 -0300

    feat: update user name from google

--- src/controller/users_google.ts
+++ src/controller/users_google.ts
@@ -26,3 +26,19 @@ export const loginGoogle: RequestHandler = async (req, res) => {
     return res.status(400).json({ error: "Erro ao fazer login com o google" });
   }
 };
+
+export const updateNameGoogle: RequestHandler = async (req: any, res) => {
+  try {
+    const { name } = req.body;
+    const newName = await UsersGoogle.findOne({
+      where: {
+        email: req.user.email,
+      },
+    });
+    await newName?.update({ name });
+    await newName?.save();
+    return res.status(200).json({ message: "Nome atualizado com sucesso" });
+  } catch (error) {
+    return res.status(404).json({ error: "Erro ao atualizar o nome" });
+  }
+};
