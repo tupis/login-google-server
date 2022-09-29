f38081550ca0307f5cfa5d0dd3b0424a25fe955b
Tupis <joaoh.tupinamba@gmail.com>
2022-09-29 01:07:26 -0300

    feat: create routes to update name and password from normal users, obs: update name from google users dont ready

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,23 @@
 import { Router } from "express";
 import { loginGoogle } from "../controller/users_google";
-import { login, register } from "../controller/users";
+import {
+  login,
+  register,
+  updateName,
+  updatePassword,
+} from "../controller/users";
 
 // MiddleWares
 import isRegisteredUser from "../middlewares/isRegisteredUser";
-// import AuthToken from "../middlewares/auth";
+import AuthToken from "../middlewares/auth";
 
 const router = Router();
 
 router.post("/login/google", loginGoogle);
 router.post("/register", isRegisteredUser, register);
 router.post("/login", login);
+// router.update("/name/google",)
+router.put("/name/", AuthToken, updateName);
+router.put("/password", AuthToken, updatePassword);
 
 export default router;
